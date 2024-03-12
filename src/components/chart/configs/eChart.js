75ab637c15701fb64dbf10b892b5de7c8822a5d4
emircan <emircankaradeniz55@gmail.com>
2024-03-12 12:02:37 +0300

    ilk commit

---
+++ src/components/chart/configs/eChart.js
@@ -0,0 +1,115 @@
+const eChart = {
+  series: [
+    {
+      name: "Tamamlanan",
+      data: [45, 20, 10, 22, 50, 10, 40, 23, 50,21,35,12],
+      color: "#fff",
+    },
+  ],
+
+  options: {
+    chart: {
+      type: "bar",
+      width: "100%",
+      height: "auto",
+
+      toolbar: {
+        show: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "55%",
+        borderRadius: 5,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      show: true,
+      width: 1,
+      colors: ["transparent"],
+    },
+    grid: {
+      show: true,
+      borderColor: "#ccc",
+      strokeDashArray: 2,
+    },
+    xaxis: {
+      categories: [
+        "Ocak",
+        "Şubat",
+        "Mart",
+        "Nisan",
+        "Mayıs",
+        "Haziran",
+        "Temmuz",
+        "Ağustos",
+        "Eylül",
+        "Ekim",
+        "Kasım",
+        "Aralık",
+      ],
+      labels: {
+        show: true,
+        align: "right",
+        minWidth: 0,
+        maxWidth: 160,
+        style: {
+          colors: [
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+          ],
+        },
+      },
+    },
+    yaxis: {
+      labels: {
+        show: true,
+        align: "right",
+        minWidth: 0,
+        maxWidth: 160,
+        style: {
+          colors: [
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+            "#fff",
+          ],
+        },
+      },
+    },
+
+    tooltip: {
+      y: {
+        formatter: function (val) {
+          return  val + " görev";
+        },
+      },
+    },
+  },
+};
+
+export default eChart;
